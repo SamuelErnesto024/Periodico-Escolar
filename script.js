8a243066807c5e7fb301522de7e25bde71363911
SamuelErnesto024 <Samropilier@gmail.com>
2025-11-14 00:07:42 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -12,22 +12,17 @@ document.addEventListener('DOMContentLoaded', () => {
         button.addEventListener('click', () => {
             
             // --- Lógica de los botones ---
-            // Primero, quita la clase 'active' de TODOS los botones
             navButtons.forEach(btn => btn.classList.remove('active'));
-            // Luego, añade la clase 'active' SOLO al botón que se presionó
             button.classList.add('active');
 
             
             // --- Lógica de las secciones ---
-            // Obtiene el ID de la sección a mostrar desde el atributo 'data-target' del botón
-            const targetId = button.dataset.target; // Ej: "informatica", "hoteleria"
+            const targetId = button.dataset.target;
 
-            // Oculta TODAS las secciones
             contentSections.forEach(section => {
                 section.classList.remove('active');
             });
 
-            // Muestra SOLAMENTE la sección correspondiente
             const targetSection = document.getElementById(targetId);
             if (targetSection) {
                 targetSection.classList.add('active');
